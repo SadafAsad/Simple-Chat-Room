fafba504ff1f842226afac68244f46560ebe4d9c
SadafAsad <sadaf98x@gmail.com>
2020-09-16 13:57:50 +0430

    client.py changed using pickle

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 HEADERSIZE = 10
 
@@ -13,7 +14,7 @@ s.connect((socket.gethostname(), 1234))
 
 while True:
     # buffering data
-    full_msg = ''
+    full_msg = b''
     new_msg = True
     while True:
         msg = s.recv(16)
@@ -22,12 +23,16 @@ while True:
             msglen = int(msg[:HEADERSIZE])
             new_msg = False
 
-        full_msg += msg.decode('utf-8')
+        full_msg += msg
 
         if len(full_msg)-HEADERSIZE == msglen:
             print("full msg recvd")
             print(full_msg [HEADERSIZE:])
+
+            d = pickle.loads(full_msg [HEADERSIZE:])
+            print(d)
+
             new_msg = True
-            full_msg = ''
+            full_msg = b''
 
 print(full_msg)
