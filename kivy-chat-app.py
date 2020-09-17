14c4c9ebb0a05b8cd30d1414a59aa50153438116
SadafAsad <sadaf98x@gmail.com>
2020-09-17 23:46:34 +0430

    start with kivy

--- kivy-chat-app.py
+++ kivy-chat-app.py
@@ -1,1 +1,14 @@
 import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class EpicApp(App):
+    # initiallization method
+    def build(self):
+        return Label(text="Hey there!")
+
+
+if __name__ == "__main__":
+    EpicApp().run()
