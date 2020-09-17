b80b9d7108de46e17fbc39c1234a9659b4fcfd4b
SadafAsad <sadaf98x@gmail.com>
2020-09-18 00:02:43 +0430

    app first page

--- kivy-chat-app.py
+++ kivy-chat-app.py
@@ -1,13 +1,39 @@
 import kivy
 from kivy.app import App
+# put texts on screen
 from kivy.uix.label import Label
+# to organize widgets
 from kivy.uix.gridlayout import GridLayout
+# text from user
 from kivy.uix.textinput import TextInput
+
+class ConnectPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+
+        # row n1
+        self.add_widget(Label(text="IP:"))
+
+        self.ip = TextInput(multiline=False)
+        self.add_widget(self.ip)
+
+        # row n2
+        self.add_widget(Label(text="Port:"))
+
+        self.port = TextInput(multiline=False)
+        self.add_widget(self.port)
+
+        # row n3
+        self.add_widget(Label(text="Username:"))
+
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
 
 class EpicApp(App):
     # initiallization method
     def build(self):
-        return Label(text="Hey there!")
+        return ConnectPage()
 
 
 if __name__ == "__main__":
